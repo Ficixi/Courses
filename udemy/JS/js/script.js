28308c9751d7aed345bf5635c2f5a61af7c46d8a
PCone <zefhuf808@gmail.com>
2022-02-16 20:28:36 +1000

    JS project

---
+++ udemy/JS/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//const answer = prompt("Введите ваше имя", "");  // в браузере появляется окно с полем для заполнения
+
+// const ask = [];
+// ask[0] = prompt('what is your name?', '');
+// ask[1] = +prompt('enter your age', '');   // '+'(унарный плюс - плюс перед аргументом) данные обрабатываются как числовые, а не строчные
+// ask[2] = prompt('your gender', '');
+// console.log(ask);
+
+
+const answer1 = confirm("да или нет?"); // появляется окно с выбором нажатия на кнопки, результат нажатия является TRUE или FALSE
+console.log(answer1);
